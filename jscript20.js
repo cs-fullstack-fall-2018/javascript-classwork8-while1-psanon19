1cf4bf6f197932ac55e3b4793d2a038c6fc460ce
Pelane Sanon <p_sanon@aol.com>
2018-07-31 15:20:27 -0500

    Submit

---
+++ jscript20.js
@@ -0,0 +1,12 @@
+function main()
+{
+    var counter = -10;
+
+    while (counter <= 5)
+    {
+        console.log(counter);
+        counter = counter + 1;
+    }
+}
+
+main()
---
+++ jscript21.js
@@ -0,0 +1,12 @@
+function main()
+{
+    var counter = -1;
+
+    while (counter< 100)
+    {
+        console.log(counter);
+        counter = counter + 2;
+    }
+}
+
+main()
